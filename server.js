95be1a934ee1ff490bc8a072a313da2f02759e4b
jalarconba <148835203+jalarconba@users.noreply.github.com>
2024-05-18 18:19:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.get('/alumnos', async (req, res) => {
     }
 });
 
+// Ruta para obtener todos los usuarios
+app.get('/usuarios', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM usuarios;');
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error al consultar la base de datos:', err);
+        res.status(500).send('Error al obtener los usuarios');
+    }
+});
 // Ruta para obtener todos los apoderados
 app.get('/apoderados', async (req, res) => {
     try {
@@ -30,79 +40,48 @@ app.get('/apoderados', async (req, res) => {
     }
 });
 
-// Ruta para obtener todos los usuarios
-app.get('/usuarios', async (req, res) => {
+// Ruta para crear un nuevo alumno
+app.post("/create", async (req, res) => {
+    const { rut, nombres, apellido_paterno, apellido_materno, correo_electronico, fecha_nacimiento, curso } = req.body;
     try {
-        const result = await pool.query('SELECT * FROM usuarios;');
-        res.json(result.rows);
+        const result = await pool.query(
+            'INSERT INTO alumnos (rut, nombres, apellido_paterno, apellido_materno, correo_electronico, fecha_nacimiento, curso) VALUES ($1, $2, $3, $4, $5, $6)', 
+            [rut, nombres, apellido_paterno, apellido_materno, correo_electronico, fecha_nacimiento, curso]
+        );
+        console.log("Alumno creado:", result);
+        res.status(201).json({ message: 'Alumno creado correctamente' });
     } catch (err) {
-        console.error('Error al consultar la base de datos:', err);
-        res.status(500).send('Error al obtener los usuarios');
+        console.error("Error al crear alumno:", err);
+        res.status(500).json({ message: 'Error al crear alumno' });
     }
 });
 
-// Ruta para crear un nuevo alumno
-app.post("/create", (req, res) => {
-    const { rut_alumno, nombre_alumno, curso, fecha, hora_llegada } = req.body;
-
-    pool.query(
-        'INSERT INTO alumnos (rut_alumno, nombre_alumno, curso, fecha, hora_llegada) VALUES ($1, $2, $3, $4, $5)',
-        [rut_alumno, nombre_alumno, curso, fecha, hora_llegada],
-        (err, result) => {
-            if (err) {
-                console.error("Error al crear alumno:", err);
-                res.status(500).json({ message: 'Error al crear alumno' });
-            } else {
-                console.log("Alumno creado:", result);
-                res.status(201).json({ message: 'Alumno creado correctamente' });
-            }
-        }
-    );
-});
-
 // Ruta para actualizar un alumno
-app.put("/update", (req, res) => {
-    const { id, rut_alumno, nombre_alumno, curso, fecha, hora_llegada } = req.body;
-
-    pool.query(
-        'UPDATE alumnos SET rut_alumno = $1, nombre_alumno = $2, curso = $3, fecha = $4, hora_llegada = $5 WHERE id = $6',
-        [rut_alumno, nombre_alumno, curso, fecha, hora_llegada, id],
-        (err, result) => {
-            if (err) {
-                console.error("Error al actualizar alumno:", err);
-                res.status(500).json({ message: 'Error al actualizar alumno' });
-            } else {
-                console.log("Alumno actualizado:", result);
-                res.status(200).json({ message: 'Alumno actualizado correctamente' });
-            }
-        }
-    );
+app.put("/update", async (req, res) => {
+    const { id, rut, nombres, apellido_paterno, apellido_materno, correo_electronico, curso } = req.body;
+    try {
+        const result = await pool.query(
+            'UPDATE alumnos SET rut = $1, nombres = $2, apellido_paterno = $3, apellido_materno = $4, correo_electronico = $5, fecha_nacimiento = $6 curso = $7 WHERE id = $8', 
+            [rut, nombres, apellido_paterno, apellido_materno, correo_electronico, fecha_nacimiento, curso, id]
+        );
+        console.log("Alumno actualizado:", result);
+        res.status(200).json({ message: 'Alumno actualizado correctamente' });
+    } catch (err) {
+        console.error("Error al actualizar alumno:", err);
+        res.status(500).json({ message: 'Error al actualizar alumno' });
+    }
 });
 
 // Ruta para eliminar un alumno
-app.delete("/delete/:id", (req, res) => {
+app.delete("/delete/:id", async (req, res) => {
     const id = req.params.id;
-
-    pool.query('DELETE FROM alumnos WHERE id = $1', [id], (err, result) => {
-        if (err) {
-            console.error("Error al eliminar alumno:", err);
-            res.status(500).json({ message: 'Error al eliminar alumno' });
-        } else {
-            console.log("Alumno eliminado:", result);
-            res.status(200).json({ message: 'Alumno eliminado correctamente' });
-        }
-    });
-});
-
-// Ruta para obtener un alumno por RUT
-app.get('/alumnos/:rut', async (req, res) => {
-    const rut = req.params.rut;
     try {
-        const result = await pool.query('SELECT * FROM alumnos WHERE rut_alumno = $1;', [rut]);
-        res.json(result.rows);
+        const result = await pool.query('DELETE FROM alumnos WHERE id = $1', [id]);
+        console.log("Alumno eliminado:", result);
+        res.status(200).json({ message: 'Alumno eliminado correctamente' });
     } catch (err) {
-        console.error('Error al consultar la base de datos:', err);
-        res.status(500).send('Error al obtener el alumno');
+        console.error("Error al eliminar alumno:", err);
+        res.status(500).json({ message: 'Error al eliminar alumno' });
     }
 });
 

e1eefabd662578b3da8aee43d7377c7e35b32046
jalarconba <148835203+jalarconba@users.noreply.github.com>
2024-05-18 01:13:50 -0400

    Update conexionDB.js

--- db/conexionDB.js
+++ db/conexionDB.js
@@ -1,12 +1,14 @@
-import pg from "pg"
+import pg from "pg";
 
 const pool = new pg.Pool({
-    host:'dpg-cp2o4g21hbls7381qot0-a',
-    user:'asistencia_up7g_user',
-    port:5432,
-    password:'zUEFUu2J8g92bRFx8Mau4uUh7INAynqD',
-    database:'asistencia_up7g',
+    host: 'dpg-cp2o4g21hbls7381qot0-a', // Revisa la dirección del servidor
+    user: 'asistencia_up7g_user',
+    port: 5432, // Puerto predeterminado de PostgreSQL
+    password: 'zUEFUu2J8g92bRFx8Mau4uUh7INAynqD',
+    database: 'asistencia_up7g', // Nombre de la base de datos
     allowExitOnIdle: true,
-    ssl: true
-})
+    ssl: true // Asegúrate de que la configuración SSL sea correcta
+});
+
 export default pool;
+
